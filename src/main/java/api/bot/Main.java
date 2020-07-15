c03cfbd1b20b6ff5dfe6bb120d5a9334986c6da1
Andrey Babichev <Al.94_94@mail.ru>
2020-07-15 15:14:50 +0300

    Test commit for heroku 1

--- src/main/java/api/bot/Main.java
+++ src/main/java/api/bot/Main.java
@@ -17,54 +17,54 @@ public class Main {
         BasicConfigurator.configure();
 
         Group group = new Group(197144715, ACCESS_TOKEN);
-
-        group.onSimpleTextMessage(message -> {
-                Message toSend = new Message()
-                        .from(group)
-                        .to(message.authorId());
-            LocalDateTime ldt = LocalDateTime.now();
-            if ((DayOfWeek.MONDAY.equals(ldt.getDayOfWeek())
-                    && ((ldt.getHour() > 18)
-                    && (ldt.getHour() <= 23))) ||
-                    (DayOfWeek.WEDNESDAY.equals(ldt.getDayOfWeek())
-                            && ((ldt.getHour() > 11)
-                            && (ldt.getHour() <= 23)))) {
-                toSend.text("У нас работает чат-бот, поэтому для получения информации введите:" + "\n"
-                        + "/работа - для получения графика работы" + "\n" +
-                "/расписание - для того, чтобы получить рассписание" + "\n" +
-                        "/праздники - получение списка праздников").send();
-            }
-        });
-
-
-        group.onCommand("/работа", message -> {
-             new Message()
-                    .from(group)
-                    .to(message.authorId())
-                    .text("Мы работаем во вторник, четверг и иногда в субботу с 12 до 18." + "\n"
-                            + "Для более подробной информации про рассписание введите /расписание")
-                    .send();
-        });
-
-//        group.onCommand("/расписание", message -> {
-//            new Message()
+//
+//        group.onSimpleTextMessage(message -> {
+//                Message toSend = new Message()
+//                        .from(group)
+//                        .to(message.authorId());
+//            LocalDateTime ldt = LocalDateTime.now();
+//            if ((DayOfWeek.MONDAY.equals(ldt.getDayOfWeek())
+//                    && ((ldt.getHour() > 18)
+//                    && (ldt.getHour() <= 23))) ||
+//                    (DayOfWeek.WEDNESDAY.equals(ldt.getDayOfWeek())
+//                            && ((ldt.getHour() > 11)
+//                            && (ldt.getHour() <= 23)))) {
+//                toSend.text("У нас работает чат-бот, поэтому для получения информации введите:" + "\n"
+//                        + "/работа - для получения графика работы" + "\n" +
+//                "/расписание - для того, чтобы получить рассписание" + "\n" +
+//                        "/праздники - получение списка праздников").send();
+//            }
+//        });
+//
+//
+//        group.onCommand("/работа", message -> {
+//             new Message()
 //                    .from(group)
 //                    .to(message.authorId())
-//                    .text("11:30 - выезжает автобус на хутор" + "\n"
-//                            + "12:00 - 15:30 - занятия по расписанию"+ "\n"
-//                            + "15:30 - 16:00 - обед" + "\n"
-//                            + "16:00 - 18:00 - занятия по расписанию" + "\n"
-//                            + "18:00 - автобус заберает всех домой" + "\n")
+//                    .text("Мы работаем во вторник, четверг и иногда в субботу с 12 до 18." + "\n"
+//                            + "Для более подробной информации про рассписание введите /расписание")
 //                    .send();
 //        });
 //
-//        group.onCommand("/праздники", message -> {
-//            new Message()
-//                    .from(group)
-//                    .to(message.authorId())
-//                    .text("Любое воскресенье для нас праздник" + "\n")
-//                    .send();
-//        });
+////        group.onCommand("/расписание", message -> {
+////            new Message()
+////                    .from(group)
+////                    .to(message.authorId())
+////                    .text("11:30 - выезжает автобус на хутор" + "\n"
+////                            + "12:00 - 15:30 - занятия по расписанию"+ "\n"
+////                            + "15:30 - 16:00 - обед" + "\n"
+////                            + "16:00 - 18:00 - занятия по расписанию" + "\n"
+////                            + "18:00 - автобус заберает всех домой" + "\n")
+////                    .send();
+////        });
+////
+////        group.onCommand("/праздники", message -> {
+////            new Message()
+////                    .from(group)
+////                    .to(message.authorId())
+////                    .text("Любое воскресенье для нас праздник" + "\n")
+////                    .send();
+////        });
 
     }
 }
